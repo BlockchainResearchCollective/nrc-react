3273b15981ad8e0ad9fbe254f43f020a92349e14
Alex Xiong <veralexxiong@outlook.com>
2017-12-27 11:20:43 +0800

    action creator and reducer for reset password

---
+++ src/components/ResetPassword.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { connect } from 'react-redux'
+
+class ResetPassword extends React.Component {
+	render(){
+		return (
+			<div>
+			hi
+			</div>
+		)
+	}
+}
+
+export default ResetPassword
